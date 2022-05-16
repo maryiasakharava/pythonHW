92b61d2b3774328a109cbf5f66cfca733d4e34e5
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:40:32 -0700

    timeit 3

--- timeit.py
+++ timeit.py
@@ -1,6 +1,7 @@
 from time import time
   
 def calculate_time(func):
+    @wraps(func)
     def timeit_wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
@@ -8,5 +9,5 @@ def calculate_time(func):
         total_time = end_time - start_time
         print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
         return result
-    return calculate_time
+   
 
