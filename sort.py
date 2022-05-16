00f7e0da41cdb1cfd45a64155962a4d6324e56e8
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:14:02 -0700

    timeit fixed

--- sort.py
+++ sort.py
@@ -1,14 +1,5 @@
 def sort_list(x):
     n=len(x)
-    if (n==0):
-        print ('Invalid input')
-        exit()
-    l=0
-    while l<n:
-        if type(x[l])== str:
-            print ('Invalid input, string detected')
-            exit()
-        l+=1
     i = 0
     while i<n:
         y = 0
--- timeit.py
+++ timeit.py
@@ -1,13 +1,19 @@
-import time
+from time import time
+from time import sleep
 from functools import wraps
 
+#function to gerate execution time
 def calculate_time(func):
     @wraps(func)
-    def calculate_time_wrapper(*args, **kwargs):
-        starting = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        total = end - starting
-        print(f'Total time {total:.4f} seconds')
-        return result
-    return calculate_time_wrapper)
+    def _give_time(*args, **kwargs):
+        start_time = int((round(time() * 1000)/1000)%60)
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_time = int((round(time() * 1000)/1000)%60)
+            execution_time=end_time-start_time
+            print(f"EXACTLY  {execution_time if execution_time > 0 else 0} second")
+    return _give_time
+
+
+
