422443dd8efbce10e0604e678d6644da10f1910c
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:56:10 -0700

    timeit fixing 100

--- timeit.py
+++ timeit.py
@@ -4,7 +4,7 @@ from time import process_time
 
   
 def calculate_time(func):
-   def inner(*args, **kwargs):
+    def inner(*args, **kwargs):
         start_time = perf_counter()
         to_execute = fn(*args, **kwargs)
         end_time = perf_counter()
