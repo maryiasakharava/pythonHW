180326aef3451f501e02c691aa66b42fb4b602f5
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:22:25 -0700

    timeit

--- timeit.py
+++ timeit.py
@@ -1,14 +1,13 @@
-from functools import wraps
 from time import time
-def calcualte_time(func):
-    @wraps(func)
-    def _time_it(*args, **kwargs):
-        starttime = int(round(time() * 1000))
-        try:
-            return func(*args, **kwargs)
-        finally:
-            end_ = int(round(time() * 1000)) - starttime
-            print(f"Total execution time: {end_ if end_ > 0 else 0} ms")
-    return calcualte_time
-
-
+  
+  
+def calculate_time(func):
+    # This function shows the execution time of
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        return result
+    return wrap_func
