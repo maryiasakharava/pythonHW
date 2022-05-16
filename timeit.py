1a64ba1c75784a1cc6e1747b4d4f78edaf003758
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:47:16 -0700

    timeit 8

--- timeit.py
+++ timeit.py
@@ -12,7 +12,7 @@ def calculate_time(func):
         finally:
             end_ = int(round(process_time() * 1000)) - start
             print(
-                f"Total execution time {func.__name__}: {end_ if end_ > 0 else 0} ms"
+                f"Total time {end_ if end_ > 0 else 0}"
             )
 
     return timeit_wrapper
