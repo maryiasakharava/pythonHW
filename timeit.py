c5c263e1eb8bd00ee3cb9bf1091d8109d111462b
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:18:08 -0700

    timeit fix

--- timeit.py
+++ timeit.py
@@ -1,19 +1,14 @@
-from time import time
-from time import sleep
 from functools import wraps
-
-#function to gerate execution time
+from time import time
 def calculate_time(func):
     @wraps(func)
-    def _give_time(*args, **kwargs):
-        start_time = int((round(time() * 1000)/1000)%60)
+    def _time_it(*args, **kwargs):
+        starttime = int(round(time() * 1000))
         try:
             return func(*args, **kwargs)
         finally:
-            end_time = int((round(time() * 1000)/1000)%60)
-            execution_time=end_time-start_time
-            print(f"EXACTLY  {execution_time if execution_time > 0 else 0} second")
-    return _give_time
-
+            end_ = int(round(time() * 1000)) - starttime
+            print(f"Total execution time: {end_ if end_ > 0 else 0} ms")
+    return calcualte_time
 
 
