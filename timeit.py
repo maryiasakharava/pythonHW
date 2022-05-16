a44b4ced8532671df16fb9403569b53fdf969d41
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:35:58 -0700

    timeit 1

--- timeit.py
+++ timeit.py
@@ -1,13 +1,14 @@
 from time import time
   
   
-def calculate_time(func):
-    # This function shows the execution time of
-    # the function object passed
-    def wrap_func(*args, **kwargs):
-        t1 = time()
+def calcualte_time(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
         return result
-    return wrap_func
+    return calcualte_time
+
