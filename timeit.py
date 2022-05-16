037cd451aec7a16fba1442a09a2b5e2931fcccd5
Maryia Sakharava <maryia.sakharava@sjsu.edu>
2022-05-16 14:43:16 -0700

    timeit 5

--- timeit.py
+++ timeit.py
@@ -4,11 +4,15 @@ from functools import wraps
 def calculate_time(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
-        return result
+        start = int(round(process_time() * 1000))
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_ = int(round(process_time() * 1000)) - start
+            print(
+                f"Total execution time {func.__name__}: {end_ if end_ > 0 else 0} ms"
+            )
+
+    return _time_it
    
 
